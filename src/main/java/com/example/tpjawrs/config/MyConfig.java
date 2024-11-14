234c507ff3c6f4139aca57b89bbe473af1afd8e8
ikram <souitaikrame00@gmail.com>
2024-11-14 20:37:44 +0100

    Ajout du projet

--- src/main/java/com/example/tpjawrs/config/MyConfig.java
+++ src/main/java/com/example/tpjawrs/config/MyConfig.java
@@ -1,16 +1,12 @@
 package com.example.tpjawrs.config;
 
-import com.example.tpjawrs.controller.CompteRestJaxRSAPI;
+import com.example.tpjawrs.controller.CompteRestController;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
-public class MyConfig {
-    @Bean
-    public ResourceConfig resourceConfig() {
-        ResourceConfig jerseyServlet = new ResourceConfig();
-        jerseyServlet.register(CompteRestJaxRSAPI.class);
-        return jerseyServlet;
+@Component
+public class MyConfig extends ResourceConfig {
+    public MyConfig() {
+        register(CompteRestController.class);
     }
 }
